30985784fe6802cf783f24d1ac3fe76d909edf21
Damon Oehlman <damon.oehlman@sidelab.com>
2013-05-03 09:38:47 +1000

    Added mouse handling

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var Stream = require('stream'),
     supportsTouch = require('feature/touch');
 
+function fakeTouch(evt) {
+    return [{
+        identifier: evt.button,
+        pageX: evt.pageX,
+        pageY: evt.pageY
+    }];
+}
+
 var capture = exports.capture = function(node, options) {
     // create the new stream
     var stream = new Stream(),
         opts = options || {},
-        target = opts.target || node;
+        target = opts.target || node,
+        isDown = false;
 
     function data(type, touches, evt) {
         touches = [].slice.call(touches).map(function(touch) {
@@ -15,20 +24,44 @@ var capture = exports.capture = function(node, options) {
         stream.emit('data', [type].concat(touches).join(':'), evt);
     }
 
-    // add the appropriate listeners
-    node.addEventListener('touchstart', function(evt) {
-        data('start', evt.changedTouches, evt);
-    });
+    // if this interface supports touch, use touch events
+    if (supportsTouch) {
+        // add the appropriate listeners
+        node.addEventListener('touchstart', function(evt) {
+            data('start', evt.changedTouches, evt);
+        });
 
-    node.addEventListener('touchmove', function(evt) {
-        data('move', evt.targetTouches, evt);
-    });
+        node.addEventListener('touchmove', function(evt) {
+            data('move', evt.targetTouches, evt);
+        });
 
-    node.addEventListener('touchend', function(evt) {
-        data('end', evt.changedTouches, evt);
-    });
+        node.addEventListener('touchend', function(evt) {
+            data('end', evt.changedTouches, evt);
+        });
+    }
+    // otherwise, capture mouse events
+    else {
+        node.addEventListener('mousedown', function(evt) {
+            isDown = isDown || (evt.button === 0);
+            data(
+                'start', fakeTouch(evt), evt);
+        });
 
-    // TODO: if the interface does not support touch, then capture mouse events and broadcast as touch
+        node.addEventListener('mousemove', function(evt) {
+            if (isDown) {
+                data('move', fakeTouch(evt), evt);
+            }
+        });
+
+        node.addEventListener('mouseup', function(evt) {
+            data('end', fakeTouch(evt), evt);
+        });
+
+        // mouse up events are handled at the document level
+        document.addEventListener('mouseup', function(evt) {
+            isDown = isDown && (evt.button !== 0);
+        });
+    }
 
     return stream;
 };
